db8c3f4a63c2be36c22d8108bc2ee18dd0c75efb
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-07-08 16:25:25 +0500

    second msg

--- security-groups.tf
+++ security-groups.tf
@@ -14,13 +14,13 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks      = ["0.0.0.0/0"]       # open to the world – tighten as needed
   }
 
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description      = "HTTP"
+  #   from_port        = 80
+  #   to_port          = 80
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  # }
 
   # Egress: allow all outbound by default
   egress {
